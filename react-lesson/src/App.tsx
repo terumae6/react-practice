1cfa50b39baaf6a93c285e14138d280312e6f66b
hikari <akiratanimoto@MacBook-Pro.local>
2020-08-10 07:23:27 +0900

    コメントの追加

--- react-lesson/src/App.tsx
+++ react-lesson/src/App.tsx
@@ -64,22 +64,28 @@ useCallback
 
 */
 
-const App: React.FC = () => {
-  const [ count, setCount ] = useState<number>(0)
+// hoge
+// hoge
+// hoge
+// hoge
 
+const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0)
+  
   const handleIncrement = useCallback(() => {
     setCount(prev => prev + 1)
   }, [])
-
+  
   const handleDecrement = useCallback(() => {
     setCount(prev => prev - 1)
   }, [])
-
+  
   return (
     <div className='App'>
-      <div>{ count }</div>
+      <div>{count}</div>
       <div>
         <button onClick={handleIncrement}>+1</button>
+        <button onClick={handleDecrement}>-1</button>
         <button onClick={handleDecrement}>-1</button>
       </div>
     </div>
